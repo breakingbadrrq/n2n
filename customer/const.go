6b3d2293b1bcbc40813ff2549b66d4eaefd72863
breakingbad_rrq <breakingbad_rrq@aliyun.com>
2018-09-26 23:36:12 +0800

    create base project

--- customer/const.go
+++
@@ -1,6 +0,0 @@
-package customer
-
-const (
-	CommonDBPath  = "root:sdg666888@tcp(192.168.50.154:3306)/common?charset=utf8"
-	LicenseMapUrl = "https://192.168.50.88/uploadfile/customerManage/companyPic/"
-)
